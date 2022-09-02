ee13213c2f3db38a1c1991a6eedc0bed46012de2
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-09-02 13:31:59 +0700

    open api

--- before-actions/index.php
+++ before-actions/index.php
@@ -10,6 +10,7 @@ if(startWith($route,'api'))
     header("Access-Control-Allow-Headers: *");
     header("Access-Control-Allow-Methods: *");
     header("Content-Type: application/json");
+    return true;
 }
 
 // check if installation is exists
