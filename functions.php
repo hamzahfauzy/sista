c0e40dbcdb22a771ed268e0119817b2555fc3d02
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-09-21 11:41:55 +0700

    fix variabel nilai

--- functions.php
+++ functions.php
@@ -571,9 +571,9 @@ function bulanIndo()
     return ['','Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'];
 }
 
-function getStatusJawaban($nilai, $i, $k)
+function getStatusJawaban($n, $i, $k)
 {
-    foreach($nilai as $nilai)
+    foreach($n as $nilai)
     {
         if($nilai->indikator->id == $i->id)
         {
