93f2bce85e0543386038fb3d870880dffd9d27cc
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-09-21 10:16:44 +0700

    update

--- actions/default/index.php
+++ actions/default/index.php
@@ -1,3 +1,25 @@
 <?php
 
 Page::set_title('Dashboard');
+
+$user = auth()->user;
+
+$periode = isset($_GET['tahun']) ? $_GET['tahun'] : date('Y');
+
+if(!in_array(get_role($user->id)->name,['administrator','bupati']))
+{
+    $petugas = $db->single('petugas',['user_id'=>$user->id]);
+    $kecamatan_id = $petugas->kecamatan_id;
+    if(!empty($petugas->kelurahan_id))
+    {
+        header('location:'.routeTo('default/kelurahan',[
+            'kelurahan_id' => $petugas->kelurahan_id,
+            'tahun' => $periode,
+        ]));
+    }
+    header('location:'.routeTo('default/kecamatan',[
+        'kecamatan_id' => $kecamatan_id,
+        'tahun' => $periode,
+    ]));
+    die();
+}
