1f3e3e43eb2f28fae0566438bed06705f15fa7cf
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-10-24 13:08:07 +0700

    fix is kk nilai

--- templates/rekapitulasi/tpl/index.php
+++ templates/rekapitulasi/tpl/index.php
@@ -14,7 +14,7 @@
         <td><?=$k->jumlah_kk?></td>
         <td><?=$k->kk_nilai?></td>
         <td><?=$k->kk_belum_nilai?></td>
-        <?php if(isset($k->kategori) && $k->kk_nilai > 0): ?>
+        <?php if(isset($k->kategori) && $k->kk_nilai != '0'): ?>
         <td style="background:<?=$k->kategori->warna?>;color:#FFF;">
             <?=$k->kategori->nama?>
         <?php else: ?>
