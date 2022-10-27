1976d1a19333fc40b1a4ff9632248f24c34180ef
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-10-27 08:21:51 +0700

    update petugas

--- actions/petugas/override-edit-fields.php
+++ actions/petugas/override-edit-fields.php
@@ -1,8 +1,9 @@
 <?php
 
-return array_merge($fields,[
-    'sebagai' => [
-        'label' => 'Sebagai',
-        'type'  => 'options-obj:roles,id,name'
-    ]
-]);
+return $fields;
+// return array_merge($fields,[
+//     'sebagai' => [
+//         'label' => 'Sebagai',
+//         'type'  => 'options-obj:roles,id,name'
+//     ]
+// ]);
